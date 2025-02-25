3bcf6e150c6689b1ef87f06bb51a41b3081d6eff
user <a1459554470@gmail.com>
2025-02-25 18:35:52 +0800

    fix compile problem

--- src/09/my_log.h
+++ src/09/my_log.h
@@ -4,42 +4,13 @@
 #define LOG_ERR(msg) print_log(__FILE__,__LINE__,ERR,msg);
 
 #include<string>
-#include<fstream>
-#include<sstream>
-#include<ctime>
 using namespace std;
 
 enum log_level{
     INFO = 1,
     ERR = 2
 };
-string current_time(){
-    time_t now = time(0);
-    tm tstruct = *localtime(&now);
-    char buf[64];
-    strftime(buf,sizeof(buf),"%Y-%m-%d %X",&tstruct);
-    return string(buf);
-}
-
-void print_log(const char *file,const int lineNumber,const log_level level, const string msg){
-    stringstream log;
-    if(INFO == level){
-        log << "INFO ";
-    }
-    else if (ERR == level)
-    {
-        log << "ERR ";
-    }
-    log << current_time() << ' ';
-    log << file << ':' << lineNumber << ' ';
-    log << msg;
-    ofstream logfile("D:\\OneDrive\\learnopengl\\src\\09\\mylog",ios::out|ios::app);
-    if(logfile.is_open()){
-        logfile << log.str() << endl;
-    }
-    else{
-        cout << log.str() << endl; 
-    }
-}
+string current_time();
+void print_log(const char *file,const int lineNumber,const log_level level, const string msg);
 
 #endif
